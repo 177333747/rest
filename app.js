3b221c0d78d77f9c8917a52e661c0c989c02e26b
Dmytro <dmitriy@example.ua>
2020-12-31 15:03:27 +0200

    comments

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const path = require('path')
+const express = require('express') //подключаем express
+const path = require('path') //для того чтобы корректно работать с путями
 const {v4} = require('uuid')
-const app = express()
+const app = express() //создаем обьект приложения
 
 
 let CONTACTS = [
     {id: v4(), value: Math.random(), marked:false}
 ]
 
+//если я добавляю эту строчку  я могу работать с реквестами
 app.use(express.json())
 
 //GET
+// api contact это route 
+// ( ) => колбек у него есть request response
 app.get('/api/contacts', (req, res) =>{
-        res.status(200).json(CONTACTS)
 
+        //200 статус запроса - говорит что у сервер все хорошо, а чтобы вернуть данные используем метод json
+        res.status(200).json(CONTACTS)
 })
 
 //POST
+//создаем элементы на сервере
 app.post('/api/contacts', (req, res)=>{
-    const contact = {...req.body, id: v4(), marked: false}
+  
+    //новый контакт это обьект где я разверну request body
+    const contact = {...req.body, id: v4(), value: Math.random(), marked: false}
     CONTACTS.push(contact)
     res.status(201).json(contact) //201 означает что элемент был создан
 
@@ -27,22 +34,34 @@ app.post('/api/contacts', (req, res)=>{
 // DELETE
 app.delete('/api/contacts/:id', (req, res) => {
     CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
-    res.status(200).json({message: 'Контакт был удален'})
+    res.status(200).json({message: 'Deleted'})
   })
 
 //PUT
 app.put('/api/contacts/:id', (req, res) => {
+  // находим индекс нашего элемента в базе данных, параметр id храниться в req.param.id 
     const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+
+    //меняем contact на request body
     CONTACTS[idx] = req.body
+
+    //возвращаем измененный контакт
     res.json(CONTACTS[idx])
   })
 
-  
+
+// Для того чтобы коректно отдавать статические файлы из клиента. Папка client - теперь статическая
+// делаем client статической для того чтобы index.html нашел frontend js
 app.use(express.static(path.resolve(__dirname, 'client')))
 
+// когда я выполняю метод get по любым роутам, екпресс 
+// будет следить за любыми гет запросами, которые у нас есть
+// path.resolve - путь к файлу index.html
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
-
-app.listen(3000, ()=> console.log("Server started..."));
+// запускам данный сервер, 3000 это порт на котором будет
+//  запущено, и  когда сервер запуститься пусть
+// в консоли покажется server started
+app.listen(3000, ()=> console.log("Server started...")); 
 
--- client/frontend.js
+++ client/frontend.js
@@ -9,32 +9,51 @@ Vue.component('loader', {
       </div>
     `
   })
-  
+
+  //создаем новый экземпляр vue
   new Vue({
-    el: '#app',
+   
+    //указываем какой элемент является корневым, указаваем селектор по которому vue может получить данный элемент
+    el: '#app', 
+   
+    // в методе data возвращаем обьект который  является моделями для нашего приложения control -value
     data() {
       return {
         loading: false,
         form: {
           value: Math.random()
         },
+
+        //массив содержащий в себе контакты
         contacts: []
       }
     },
      
     methods: {
+      
       async generate() {
+        // с помощью деструктуризации создаем новый обьект contactи с помощью оператора rest соберем все значения которые находятся в this form
         const {...contact} = this.form
-        
+
+        //сначала делаем запрос на сервер для того чтобы сервер нам создал эти данные
         const newContact = await request('/api/contacts', 'POST', contact)
-  
+
+        //пока мы не работаем с сервером добавляем его в текущий массив
         this.contacts.push(newContact)
        
         
       },
+
+      //метод принимает id
       async retrieve(id) {
+
+        // находим текущий контакт
         const contact = this.contacts.find(c => c.id === id)
+
+        //ждем пока выполниться request по адресу и указываем что это метод put
         const updated = await request(`/api/contacts/${id}`, 'PUT', {
+
+        //с помощью оператора spread разверну текущий контакт
           ...contact,
           marked: true
         })
@@ -42,26 +61,42 @@ Vue.component('loader', {
       },
       async removeContact(id) {
         await request(`/api/contacts/${id}`, 'DELETE')
+        
+        //Если contact id = тому id который мы передавали в remove contact то это означает что нужно удалить этот элемент
         this.contacts = this.contacts.filter(c => c.id !== id)
       }
     },
+   
+    // вызывается тогда когда компонент готов
     async mounted() {
       this.loading = true
+      
+      // ждем пока функция request выполнитьс указываем url по которому делается запрос, contacts- это сущность с которой мы работаем
       this.contacts = await request('/api/contacts')
       this.loading = false
     }
   })
   
+  //функция делаем асинхронные запросы на серверпараметры: url по которому делаем запрос и передаем метод get по умолчанию, и обьект дата если она нужна
   async function request(url, method = 'GET', data = null) {
     try {
+      
+      //header это метадынные которые говорят что с этим запросом происходит
       const headers = {}
+      
+      // let потому что мы будем его переопределять
       let body
-  
+
+      //если параметр data не пустой, то я передам 1 header. тип контента который мы передаем это json
       if (data) {
         headers['Content-Type'] = 'application/json'
+
+        //stringify сериализует обьект
         body = JSON.stringify(data)
       }
-  
+     
+      // метод fetch дает возможность делать ajax запросы, первым параметром он принимает url, а вторым обьект конфигурации который позволяет законфигурировать даннный запрос
+     //  получаем обьект response и подождем когда промис fetch закончится
       const response = await fetch(url, {
         method,
         headers,
@@ -69,6 +104,8 @@ Vue.component('loader', {
       })
       return await response.json()
     } catch (e) {
+      
+      // если произошла ошибка выводим
       console.warn('Error:', e.message)
     }
   }
